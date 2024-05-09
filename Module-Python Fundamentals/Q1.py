1fbe1d4378d4952be22e2147b9bcbbf4e5bbd458
NamrataBhagtani <bnamrata.2n@gmail.com>
2024-05-10 00:25:22 +0530

    Upload assignment

---
+++ Module-Python Fundamentals/Q1.py
@@ -0,0 +1,9 @@
+#  Write a python program to sum of the first n positive integers.
+num1 = int(input("Enter the first number"))
+
+num2 = int(input("Enter the last number"))
+
+sum = (num2*(num1 + num2))/2
+
+print(sum)
+
